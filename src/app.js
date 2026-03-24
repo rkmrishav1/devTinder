e17c442fd1e033ad464333a39ab95d3ae91c905c
Rishav Kumar <rkrishavmishra@gmail.com>
2026-03-24 08:21:19 +0530

    Reading the Query Params and Dynamic Routes

--- src/app.js
+++ src/app.js
@@ -2,45 +2,22 @@ const express = require("express");
 
 const app = express(); //Creating a server/application instance.
 
-// app.use("/",(req, res) => {
-//     res.end("Namaste Rishav!");
-// });
-
-// app.use("/hello/2", (req, res) => {
-//     res.end("This is different from the first /hello route!");
-// });// --> we will get : response of /hello/2.
-
-
-// app.use("/hello", (req, res) => {
-//     res.end("Hello hello hello!");
-// });//Request handler for /hello.
-
-// app.use("/hello/2", (req, res) => {
-//     res.end("This is different from the first /hello route!");
-// });// --> we will still get : response of /hello.
-
-// * This will only handle GET call to /user
+// * work for :- /user?userid=123&name=Rishav&password=123456 (Here each parameter is required) - Query Parameters
 app.get("/user", (req, res) => {
+    console.log(req.query.userid);
+    console.log(req.query);
     res.send({firstName : "Rishav", lastName : "Mishra"});
 });
 
-app.post("/user", (req, res) => {
-    // Saving data to DB.
-    res.send("Data saved successfully!");
+// * work for :- /user/123/Rishav/123456 (Here each parameter is required) - Route Parameters
+app.get("/user/:userid/:name/:password", (req, res) => {
+    console.log(req.params);
+    console.log(req.params.userid);
+    console.log(req.params.name);
+    console.log(req.params.password);
+    res.send({firstName : "Rishav", lastName : "Mishra"});
 });
 
-app.delete("/user", (req, res) => {
-    // Deleting data from DB.
-    res.send("Data deleted successfully!");
-});
-// * This will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.end("Hello from the server!");
-}); //Request handler
-
-// app.use("/",(req, res) => {
-//     res.end("Namaste Rishav!");
-// });
 
 app.listen(7777,() =>{
     console.log("Server is successfully listening on port 7777...");
