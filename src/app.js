6181be54ac5a7a6ec876b3b335b8acfc09d5ce95
Rishav Kumar <rkrishavmishra@gmail.com>
2026-03-26 06:26:36 +0530

    writing-auth-middleware(Need of middlewares)

--- src/app.js
+++ src/app.js
@@ -2,26 +2,76 @@ const express = require("express");
 
 const app = express(); //Creating a server/application instance.
 
-// GET/ user => It checks all the app.xxx (matching route) functions.
+// & Now this more cleaner way to use Middleware for admin authorization.
+// & Make Middleware file --> export it to the main file --> use it wherever needed;
 
-// If you are sending a request to the express server. It will try to go one by one check all these methods(use, get) if it finds its matchings it will call that function and it will keep on going one after the other to these route handlers and middlewares till it reaches the function which actually sends the response back and actually that function is known as request handler.
-app.use("/", (req, res, next) => {
-    //res.send("Handling / route");
-    next();
+const{ adminAuth, userAuth } = require("./middlewares/auth");
+app.use("/admin", adminAuth);
+app.use("/user", userAuth);
+
+// & I want to write this middleware for all the request which are coming to /admin
+// //Handle Auth Middleware for all request GET, POST, PATCH, ....
+
+// app.use("/admin", (req, res, next) => {
+//     const token = "xyaaz";
+//     const isAdminAuthorized = token === "xyz";
+//     if(!isAdminAuthorized) {
+//         res.status(401).send("Unauthorized request");
+//     } else {
+//         next();
+//     }
+// });
+
+// * another way of using middleware.
+app.get("/user", userAuth, (req, res) => {
+    res.send("User data sent");
 });
 
-app.get("/user", (req, res, next) => {
-    //res.send("Handling /user route");
-    next();
-},(req, res, next) => {
-    next();
-},(req, res, next) => {
-    res.send("2nd Route Handler");
+// Here we don't need to putrestriction on user to go through authentication. So we will not put user auth middleware here.
+app.post("/user/login",(req, res) =>{
+    res.send("User logged in successfully");
 });
 
+app.post("/user/data", userAuth, (req, res) =>{
+    res.send("User data sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+    res.send("All data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+    res.send("Deleted a user");
+});
+
+
+ // & If I have to make all the admin API's authorization then in this way i will have to put authorization to    all the admin API's. --> This is not a good way. --> This is where middleware come into picture.
+
+// app.get("/admin/getAllData", (req, res) => {
+//     // * Logic of checking if the request is authorized.
+//     const token = "xyzkejf";
+//     const isAdminAuthorized = token === "xyz";
+//     if (isAdminAuthorized) {
+//         res.send("All data sent");
+//     }
+//     else {
+//         res.status(401).send("Unauthorized request");
+//     }
+// });
+
+
+// app.get("/admin/deleteUser", (req, res) => {
+//     // * Logic of checking if the request is authorized.
+//     const token = "xyzkejf";
+//     const isAdminAuthorized = token === "xyz";
+//     if (isAdminAuthorized) {
+//         res.send("Delete a user");
+//     }
+//     else {
+//         res.status(401).send("Unauthorized request");
+//     }
+// });
 
 app.listen(7777,() =>{
     console.log("Server is successfully listening on port 7777...");
 });// Listening on port 7777
-
-//npm i -g nodemon --> automatically refresh the server whenever i will make any changes to my code.
---
+++ src/middlewares/auth.js
@@ -0,0 +1,31 @@
+
+//Handle Auth Middleware for all request GET, POST, PATCH, ....
+
+const adminAuth = (req, res, next) => {
+    console.log("Admin auth is getting checked!!");
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if(!isAdminAuthorized) {
+        res.status(401).send("Unauthorized request");
+    } else {
+        next();
+    }
+};
+
+const userAuth = (req, res, next) => {
+    console.log("User auth is getting checked!!");
+    const token = "xyz";
+    const isUserAuthorized = token === "xyz";
+    if(!isUserAuthorized) {
+        res.status(401).send("Unauthorized request");
+    } else {
+        next();
+    }
+};
+
+
+
+module.exports ={
+    adminAuth,
+    userAuth
+};
