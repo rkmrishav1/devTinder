23123fc440cdb4a2c8174657492c624e35ad04bc
Rishav Kumar <rkrishavmishra@gmail.com>
2026-04-07 10:36:05 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -7,21 +7,19 @@ const User = require("./models/user");
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
+app.use(express.json()); // Parse JSON request bodies. It is a middleware
+// & This express.json middleware will read the json object, it will convert it into a JavaScript object and will add it to the req.body. Now i can read this body, get the data
+
 app.post("/signup", async (req, res) => {
+    
+    const user = new User(req.body);// & now req.body is 
     // const userObj = {
     //     firstName : "Rishav",
     //     lastName : "Mishra",
     //     emailID : "rkrishavmishra@gmail.com",
     //     password : "hello@1234",
     // }
-    //const user = new User(userObj);
-    const user = new User({
-        firstName : "Virat",
-        lastName : "Kohli",
-        emailId : "viratkohli@gmail.com",
-        password : "virat@1234",
-    });
-
+    console.log(req.body);
     try{
         await user.save()
         res.send("User added Successfully...");
